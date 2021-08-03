30acb04b7c99952cb91eadd0fbab455d0e3a9128
AlexeyMikhienkov <a.mikhienkov@yandex.ru>
2021-08-03 13:07:17 +0300

    Частично описал работу внешнего скрипта и контроллера

--- controller/inputController.js
+++ controller/inputController.js
@@ -1,38 +1,60 @@
 class inputController {
-    _enabled = false;
-    _focused = false;
-    _ACTION_ACTIVATED = 'input-controller:action-activated';
-    _ACTION_DEACTIVATED = 'input-controller:action-deactivated';
-    actions = {};
+    enabled = false;
+    focused = false;
+    ACTION_ACTIVATED = 'input-controller:action-activated';
+    ACTION_DEACTIVATED = 'input-controller:action-deactivated';
+    actions;
+    target;
 
     constructor(actionsToBind, target) {
-            this.actions = actionsToBind;
-            this.target = target;
+        this.actions = actionsToBind;
+        this.target = target;
+
+        return this;
     };
 
     bindActions(actionsToBind) {
 
     };
 
-    enableAction() {
+    enableAction(actionName: string) {
+        if (!this.isActionActive(actionName)) {
+            const act = this.actions[actionName];
+            act.enabledAction = true;
+        }
 
+        let event = new CustomEvent(this.ACTION_ACTIVATED, {
+            action: actionName
+        });
+        document.dispatchEvent(event);
     };
 
-    disableAction() {
+    disableAction(actionName: string) {
+        if (this.isActionActive(actionName)) {
+            const act = this.actions[actionName];
+            act.enabledAction = false;
+        }
 
+        let event = new CustomEvent(this.ACTION_DEACTIVATED, {
+            action: actionName
+        });
+        document.dispatchEvent(event);
     };
 
-    attach() {
-
+    attach(target, dontEnable) {
+        if (!dontEnable) {
+            this.focused = true;
+        }
     };
 
-    detach() {
-
+   detach() {
+        this.focused = false;
     };
 
-    // action - название активности (искать среди текущих активностей объекта)
+    //TODO: протестировать работу
     isActionActive(action: string) {
-        return this.actions.includes(action);
+        const act = this.actions[action];
+        return act.enabledAction;
     };
 
     isKeyPressed(keyCode: number) {
@@ -41,6 +63,10 @@ class inputController {
             //TODO: проверить, нажата ли кнопка в контроллере
         }
     };
+
+    getActions() {
+        return this.actions;
+    }
 
 
 }
--- controller/inputControllerTest.js
+++ controller/inputControllerTest.js
@@ -1,3 +1,114 @@
+let actions = {
+    "left": {
+        keyCodes: [37, 65],
+        enabledAction: false
+    },
+    "right": {
+        keyCodes: [39, 68],
+        enabledAction: false
+    },
+    "up": {
+        keyCodes: [38, 87],
+        enabledAction: false
+    },
+    "down": {
+        keyCodes: [40, 83],
+        enabledAction: false
+    }
+};
+
+const target = document.querySelector(".element");
+const controller = inputController(actions, target);
+
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+    if (!controller.enabled) {
+        alert("Контроллер отключен. Для обработки нажатия сначала включите контроллер");
+    } else {
+        const code = e.keyCode;
+        for (const action of controller.actions) {
+            const keyCodes = action.keyCodes;
+            if (keyCodes.includes(code)) {
+                controller.enableAction(action);
+            } else {
+                alert("Нет действия для данной клавиши");
+            }
+        }
+    }
+}
+
+document.addEventListener("keyup", handleKeyUp);
+
+function handleKeyUp(e) {
+    if (!controller.enabled) {
+        alert("Контроллер отключен. Для обработки нажатия сначала включите контроллер");
+    } else {
+        const code = e.keyCode;
+        for (const action of controller.actions) {
+            const keyCodes = action.keyCodes;
+            if (keyCodes.includes(code)) {
+                controller.disableAction(action);
+            } else {
+                alert("Нет действия для данной клавиши");
+            }
+        }
+    }
+
+
+}
+
+document.addEventListener("input-controller:activate", move);
+
+function move(e) {
+    const action = e.action;
+    const x = Number(action === "right") * 2 - 1;
+    const y = Number(action === "up") * 2 - 1;
+
+}
+
+
+/*
+let position = 0;
+
+function move(direction) {
+    position += direction * 500;
+    element.style.transform = `translate(${position}px)`;
+}
+
+document.addEventListener("input-controller:activate", ({detail:{action}}) => {
+    move(Number(action === "right") * 2 - 1);
+ */
+function createNewEventKeyDown(e) {
+    const eventActivate = new CustomEvent("input-controller:activate", {
+        detail: {
+            keyCode: e.keyCode
+        }
+    });
+    document.dispatchEvent(eventActivate);
+}
+
+
+
+document.addEventListener("input-controller:activate", activateController);
+
+document.addEventListener("keydown", createNewEventKeyUp);
+
+function createNewEventKeyUp() {
+    const eventDeactivate = new CustomEvent("input-controller:deactivate", {
+        detail: {
+        }
+    });
+    document.dispatchEvent(eventDeactivate);
+}
+
+function deactivateController() {
+
+}
+
+document.addEventListener("input-controller:deactivate", deactivateController);
+
+/*
 document.addEventListener("keydown", createNewEventKeyDown);
 
 function createNewEventKeyDown(e) {
@@ -9,8 +120,6 @@ function createNewEventKeyDown(e) {
     document.dispatchEvent(event);
 }
 
-const element = document.querySelector(".element");
-
 let position = 0;
 
 function move(direction) {
@@ -21,3 +130,4 @@ function move(direction) {
 document.addEventListener("input-controller:activate", ({detail:{action}}) => {
     move(Number(action === "right") * 2 - 1);
 });
+*/
